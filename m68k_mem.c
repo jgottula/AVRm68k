1a8b03c7a9265525f2a366cefb9673013b4f4dd7
Justin Gottula <justin@jgottula.com>
2012-07-01 22:13:55 -0700

    Emulator: added useful memory debug info since ram isn't present yet

--- m68k_mem.c
+++ m68k_mem.c
@@ -1,13 +1,29 @@
 #include "m68k_mem.h"
+#include "debug.h"
+#include "uart.h"
 
 /* WARNING: we may need to do endianness conversions on these */
 
 uint32_t memRead(uint32_t addr, uint8_t size)
 {
-	return 0;
+	dbgHeader();
+	uartWritePSTR("memRead: reading from 0x");
+	uartWriteHex32(addr, false);
+	uartWritePSTR(" with size 0b");
+	uartWriteBin4(size);
+	uartWritePSTR("!\n");
+	
+	return 0xdeadbeef;
 }
 
 void memWrite(uint32_t addr, uint8_t size, uint32_t value)
 {
-	
+	dbgHeader();
+	uartWritePSTR("memWrite: writing 0x");
+	uartWriteHex32(value, false);
+	uartWritePSTR(" at 0x");
+	uartWriteHex32(addr, false);
+	uartWritePSTR(" with size 0b");
+	uartWriteBin4(size);
+	uartWritePSTR("!\n");
 }
