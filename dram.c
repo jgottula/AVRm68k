53cf986dcdf9d08fe4904c286add73b317f5e7f2
Justin Gottula <justin@jgottula.com>
2012-07-04 00:49:44 -0700

    DRAM: implemented refresh cycle

--- dram.c
+++ dram.c
@@ -1,10 +1,23 @@
 #include "dram.h"
 #include <util/delay.h>
+#include "intr.h"
 #include "io.h"
 #include "uart.h"
 
 /* TODO: refresh! */
 /* TODO: fast page mode! */
+
+static void dramLoadAddrL(uint32_t addr)
+{
+	writeIO(&PORT_ADDRL, ADDRL_ALL, (uint8_t)addr);
+	writeIO(&PORT_ADDRH, ADDRH_ALL, (uint8_t)(addr >> 2));
+}
+
+static void dramLoadAddrH(uint32_t addr)
+{
+	writeIO(&PORT_ADDRL, ADDRL_ALL, (uint8_t)(addr >> 10));
+	writeIO(&PORT_ADDRH, ADDRH_ALL, (uint8_t)(addr >> 12));
+}
 
 uint8_t dramRead(uint32_t addr)
 {
@@ -14,20 +27,15 @@ uint8_t dramRead(uint32_t addr)
 	writeIO(&DDR_DATA, DATA_ALL, 0);
 	writeIO(&PORT_DATA, DATA_ALL, DATA_ALL);
 	
-	/* don't assert the write enable line */
-	writeIO(&PORT_DRAM, DRAM_WE, DRAM_WE);
-	
 	/* put the high part of the address as the row number */
-	writeIO(&PORT_ADDRL, ADDRL_ALL, (uint8_t)(addr >> 10));
-	writeIO(&PORT_ADDRH, ADDRH_ALL, (uint8_t)(addr >> 12));
+	dramLoadAddrH(addr);
 	
 	/* assert RAS and wait for it to load */
 	writeIO(&PORT_DRAM, DRAM_RAS, 0);
 	_delay_us(1);
 	
 	/* put the low part of the address as the column number */
-	writeIO(&PORT_ADDRL, ADDRL_ALL, (uint8_t)addr);
-	writeIO(&PORT_ADDRH, ADDRH_ALL, (uint8_t)(addr >> 2));
+	dramLoadAddrL(addr);
 	
 	/* assert CAS and wait for it to load */
 	writeIO(&PORT_DRAM, DRAM_CAS, 0);
@@ -40,6 +48,50 @@ uint8_t dramRead(uint32_t addr)
 	writeIO(&PORT_DRAM, DRAM_RAS | DRAM_CAS, DRAM_RAS | DRAM_CAS);
 	
 	return byte;
+}
+
+void dramWrite(uint32_t addr, uint8_t byte)
+{
+	uartWritePSTR("dramWrite: guessing DRAM timings!\n");
+	/* remember to unset WE when done */
+}
+
+void dramRefresh(void)
+{
+	uartWritePSTR("dramRefresh: guessing DRAM timings!\n");
+	uartWritePSTR("dramRefresh: start\n");
+	
+	uint16_t start = msec;
+	
+	for (uint8_t i = 0b00; i <= 0b11; ++i)
+	{
+		uint8_t j = 0x00;
+		
+		/* load the high part of the address */
+		writeIO(&PORT_ADDRH, ADDRH_ALL, i << 6);
+		
+		do
+		{
+			/* load the low part of the address */
+			writeIO(&PORT_ADDRL, ADDRL_ALL, j);
+			
+			/* assert RAS, wait, and then reset RAS */
+			writeIO(&PORT_DRAM, DRAM_RAS, 0);
+			_delay_us(1);
+			writeIO(&PORT_DRAM, DRAM_RAS, DRAM_RAS);
+			////// put another delay here??
+		}
+		while (++j != 0x00);
+	}
+	
+	uint16_t finish = msec;
+	
+	uartWritePSTR("dramRefresh: finish\n");
+	
+	uint16_t time = finish - start;
+	uartWritePSTR("dramRefresh: took ");
+	uartWriteDec16(time);
+	uartWritePSTR(" ms\n");
 }
 
 void dramInit(void)
