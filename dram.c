2e4c65a43e7f647bd436865a28399eff38981bcf
Justin Gottula <justin@jgottula.com>
2012-07-04 00:34:17 -0700

    DRAM: completed implementation of read function

--- dram.c
+++ dram.c
@@ -1,21 +1,14 @@
 #include "dram.h"
+#include <util/delay.h>
 #include "io.h"
+#include "uart.h"
 
 /* TODO: refresh! */
-
-
-
-#include "uart.h"
-#define writeIO_DEBUG(_reg, _mask, _val) \
-{ \
-	uartWriteStr(#_reg ": "); \
-	uartWriteHex8(_val & _mask, false); \
-	uartWriteChr('\n'); \
-}
+/* TODO: fast page mode! */
 
 uint8_t dramRead(uint32_t addr)
 {
-	uartWritePSTR("dramRead\n");
+	uartWritePSTR("dramRead: guessing DRAM timings!\n");
 	
 	/* set the data bus to input with pull-up */
 	writeIO(&DDR_DATA, DATA_ALL, 0);
@@ -25,16 +18,28 @@ uint8_t dramRead(uint32_t addr)
 	writeIO(&PORT_DRAM, DRAM_WE, DRAM_WE);
 	
 	/* put the high part of the address as the row number */
-	writeIO_DEBUG(&PORT_ADDRL, ADDRL_ALL, (uint8_t)(addr >> 10));
-	writeIO_DEBUG(&PORT_ADDRH, ADDRH_ALL, (uint8_t)(addr >> 12));
+	writeIO(&PORT_ADDRL, ADDRL_ALL, (uint8_t)(addr >> 10));
+	writeIO(&PORT_ADDRH, ADDRH_ALL, (uint8_t)(addr >> 12));
 	
-	/* assert RAS */
+	/* assert RAS and wait for it to load */
+	writeIO(&PORT_DRAM, DRAM_RAS, 0);
+	_delay_us(1);
 	
 	/* put the low part of the address as the column number */
-	writeIO_DEBUG(&PORT_ADDRL, ADDRL_ALL, (uint8_t)addr);
-	writeIO_DEBUG(&PORT_ADDRH, ADDRH_ALL, (uint8_t)(addr >> 2));
+	writeIO(&PORT_ADDRL, ADDRL_ALL, (uint8_t)addr);
+	writeIO(&PORT_ADDRH, ADDRH_ALL, (uint8_t)(addr >> 2));
 	
-	return 0;
+	/* assert CAS and wait for it to load */
+	writeIO(&PORT_DRAM, DRAM_CAS, 0);
+	_delay_us(1);
+	
+	/* read from the data bus */
+	uint8_t byte = readIO(&PORT_DATA, DATA_ALL);
+	
+	/* reset RAS and CAS */
+	writeIO(&PORT_DRAM, DRAM_RAS | DRAM_CAS, DRAM_RAS | DRAM_CAS);
+	
+	return byte;
 }
 
 void dramInit(void)
