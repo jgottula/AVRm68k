9bd0db571108b26943c220f58ca5f30829b7e639
Justin Gottula <justin@jgottula.com>
2012-07-06 18:50:41 -0700

    DRAM: fixed missed 10-bit addr; added correct initialization routine

--- dram.c
+++ dram.c
@@ -9,10 +9,10 @@
 /* WARNING: because the refresh routine is called from an ISR and could
  * interrupt at any time, these functions MUST use ATOMIC_BLOCKs */
 
-static void dramLoadAddrBus(uint16_t addr10)
+static void dramLoadAddrBus(uint16_t addr12)
 {
-	writeIO(&PORT_ADDRL, ADDRL_ALL, (uint8_t)addr10);
-	writeIO(&PORT_ADDRH, ADDRH_ALL, (uint8_t)(addr10 >> 2));
+	writeIO(&PORT_ADDRL, ADDRL_ALL, (uint8_t)addr12);
+	writeIO(&PORT_ADDRH, ADDRH_ALL, (uint8_t)(addr12 >> 4));
 }
 
 uint8_t dramRead(uint32_t addr)
@@ -26,7 +26,7 @@ uint8_t dramRead(uint32_t addr)
 		writeIO(&PORT_DATA, DATA_ALL, 0);
 		
 		/* put the high part of the address as the row number */
-		dramLoadAddrBus(addr >> 10);
+		dramLoadAddrBus(addr >> 12);
 		
 		/* assert RAS and wait for it to load */
 		writeIO(&PORT_DRAM, DRAM_RAS, 0);
@@ -60,7 +60,7 @@ void dramWrite(uint32_t addr, uint8_t byte)
 		writeIO(&PORT_DATA, DATA_ALL, byte);
 		
 		/* put the high part of the address as the row number */
-		dramLoadAddrBus(addr >> 10);
+		dramLoadAddrBus(addr >> 12);
 		
 		/* assert RAS and wait for it to load */
 		writeIO(&PORT_DRAM, DRAM_RAS, 0);
@@ -83,7 +83,7 @@ void dramWrite(uint32_t addr, uint8_t byte)
 
 void dramRefresh(void)
 {
-	/* this function doesn't need to save state because the non-ISR dram
+	/* this function doesn't need to save/restore state because the non-ISR dram
 	 * functions are wrapped in ATOMIC_BLOCKs */
 	
 	for (uint8_t i = 0b0000; i <= 0b1111; ++i)
@@ -116,4 +116,14 @@ void dramInit(void)
 	
 	/* deactivate all control lines */
 	writeIO(&PORT_DRAM, DRAM_ALL, DRAM_ALL);
+	
+	/* delay on init as required by the spec */
+	_delay_us(200);
+	
+	/* run 8 refresh cycles (spec) */
+	for (uint8_t i = 8; i != 0; --i)
+	{
+		_delay_ms(DRAM_REFRESH_FREQ);
+		dramRefresh();
+	}
 }
