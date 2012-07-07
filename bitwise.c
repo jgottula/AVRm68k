d0e00f9b2c04b49c79bc34c54ba49039351de19a
Justin Gottula <justin@jgottula.com>
2012-07-07 00:39:04 -0700

    Bitwise: added 16-to-32 bit sign extension function

--- bitwise.c
+++ bitwise.c
@@ -26,3 +26,11 @@ uint32_t signExtend8to32(uint8_t byte)
 	else
 		return byte;
 }
+
+uint32_t signExtend16to32(uint16_t word)
+{
+	if ((int16_t)word < 0)
+		return 0xffff0000 | word;
+	else
+		return word;
+}
--- bitwise.h
+++ bitwise.h
@@ -8,5 +8,6 @@ uint16_t decodeBigEndian16(const uint8_t *bytes);
 uint32_t decodeBigEndian32(const uint8_t *bytes);
 uint16_t signExtend8to16(uint8_t byte);
 uint32_t signExtend8to32(uint8_t byte);
+uint32_t signExtend16to32(uint16_t word);
 
 #endif
