112c3446e71cb6d3c7700c9246f304a89fc711f5
Justin Gottula <justin@jgottula.com>
2012-07-09 01:02:47 -0700

    SRAM: implemented read and write for single bytes

---
+++ sram.c
@@ -0,0 +1,80 @@
+#include "sram.h"
+#include "io.h"
+#include "spi.h"
+
+static uint8_t currentSR = SRAM_SR_MODE_BYTE;
+
+static void setMode(uint8_t mode)
+{
+	/* this function should only be called after an instruction sequence has
+	 * been started; it will reopen an instruction sequence when finished */
+	
+	if (currentSR != mode)
+	{
+		/* write to the status register */
+		spiByte(SRAM_INSTR_WRSR);
+		spiByte(mode);
+		
+		currentSR = mode;
+		
+		/* start another instruction sequence */
+		spiEnd();
+		spiBeginQuick(SPI_SS_SRAM);
+	}
+}
+
+uint8_t sramReadByte(uint16_t addr)
+{
+	uint8_t byte;
+	
+	/* start an instruction sequence */
+	spiBegin(SPI_SS_SRAM, SPIMODE_SRAM, SPIENDIAN_SRAM, SPIDIV_SRAM);
+	
+	/* set to byte mode (if necessary) */
+	setMode(SRAM_SR_MODE_BYTE);
+	
+	/* issue the read instruction */
+	spiByte(SRAM_INSTR_READ);
+	
+	/* write the address */
+	spiByte(addr >> 8);
+	spiByte(addr);
+	
+	/* read the data */
+	byte = spiByte(0x00);
+	
+	spiEnd();
+	
+	return byte;
+}
+
+void sramWriteByte(uint16_t addr, uint8_t byte)
+{
+	/* start an instruction sequence */
+	spiBegin(SPI_SS_SRAM, SPIMODE_SRAM, SPIENDIAN_SRAM, SPIDIV_SRAM);
+	
+	/* set to byte mode (if necessary) */
+	setMode(SRAM_SR_MODE_BYTE);
+	
+	/* issue the write instruction */
+	spiByte(SRAM_INSTR_WRITE);
+	
+	/* write the address */
+	spiByte(addr >> 8);
+	spiByte(addr);
+	
+	/* write the data */
+	spiByte(byte);
+	
+	spiEnd();
+}
+
+void sramReadSeq(uint16_t addr, uint16_t len, uint8_t *dest)
+{
+	
+}
+
+void sramWriteSeq(uint16_t addr, uint16_t len, const uint8_t *src)
+{
+	
+}
---
+++ sram.h
@@ -0,0 +1,24 @@
+#ifndef JGOTTULA_SRAM_H
+#define JGOTTULA_SRAM_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/* 32 KiB */
+#define SRAM_SIZE 0x8000
+
+#define SRAM_SR_MODE_BYTE 0b00000001
+#define SRAM_SR_MODE_SEQ  0b01000001
+#define SRAM_SR_MODE_PAGE 0b10000001
+
+#define SRAM_INSTR_RDSR 0b00000101
+#define SRAM_INSTR_WRSR 0b00000001
+#define SRAM_INSTR_READ 0b00000011
+#define SRAM_INSTR_WRITE 0b00000010
+
+uint8_t sramReadByte(uint16_t addr);
+void sramWriteByte(uint16_t addr, uint8_t byte);
+void sramReadSeq(uint16_t addr, uint16_t len, uint8_t *dest);
+void sramWriteSeq(uint16_t addr, uint16_t len, const uint8_t *src);
+
+#endif
