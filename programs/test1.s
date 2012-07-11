83ec1517a0790aaca0a1e56607f682b8b3702067
Justin Gottula <justin@jgottula.com>
2012-07-11 15:38:41 -0700

    Programs: implemented conditional branch test

--- programs/test1.s
+++ programs/test1.s
@@ -6,47 +6,30 @@
 	/* init stack ptr */
 	lea (0x40),%sp
 	
-	moveq #-0x40,%d0
-	movea.l (0x3c),%a6
+	move.l #0x55555555,%d0
+	move.l #0x55555555,%d1
+	move.l #0x55555555,%d2
+	move.l #0x55555555,%d3
 	
-	moveq #-0x01,%d0
-	move.w %d0,%ccr
-	move.w %sr,%d1   
-	move.w %ccr,%d1
+	andi.l #0x00000000,%d1
 	
-	movea.l (0x00000000),%a5
-	move.l (0x00000000),%a6
 	
-	move.l #0x05050505,%d3
-	move.b #0x66,%d4
-	move.l %d2,%a5
+	/*.word EMU_DUMPREG*/
 	
-	move.l #0xaaaaaaaa,%d6
-	not.l %d6
+	beq equal
+	bne nequal
+	nop
+	nop
+	nop
+	.word 0xffff /* die here */
 	
-	bsr subroutine
+equal:
+	eor.l #0x00000000,%d0
 	bra done
 	
-subroutine:
-	st  0x00
-	sf  0x01
-	shi 0x02
-	sls 0x03
-	scc 0x04
-	scs 0x05
-	sne 0x06
-	seq 0x07
-	
-	svc 0x08
-	svs 0x09
-	spl 0x0a
-	smi 0x0b
-	sge 0x0c
-	slt 0x0d
-	sgt 0x0e
-	sle 0x0f
-	
-	rts
+nequal:
+	and.l #0x00000000,%d0
+	bra done
 	
 done:
 	.word EMU_DUMPREG
