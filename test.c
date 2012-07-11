0416881fc703bb49eebc025e59cdf13e61dc3eb9
Justin Gottula <justin@jgottula.com>
2012-07-11 15:35:42 -0700

    Unit tests: added a bunch of SRAM/DRAM tests

--- test.c
+++ test.c
@@ -1,4 +1,6 @@
 #include "test.h"
+#include <string.h>
+#include <util/atomic.h>
 #include <util/delay.h>
 #include "debug.h"
 #include "dram.h"
@@ -14,7 +16,6 @@ static void testSRAM(void)
 	
 	uartWritePSTR("sram byte mode: ");
 	sramWriteByte(0x100, 0x55);
-	
 	if (sramReadByte(0x100) != 0x55)
 		fail = true;
 	sramWriteByte(0x100, 0xaa);
@@ -38,19 +39,79 @@ static void testSRAM(void)
 		uartWritePSTR("passed\n");
 }
 
+static void benchmarkSRAM(void)
+{
+	uint16_t before, after;
+	
+	before = msec;
+	for (uint32_t i = 0x0000; i < 0x0400; ++i)
+		sramReadByte(i);
+	after = msec;
+	
+	uartWritePSTR("sram byte read speed: 1024 B / ");
+	uartWriteDec16(after - before);
+	uartWritePSTR(" ms\n");
+	
+	before = msec;
+	uint8_t dest[0x400];
+	sramReadSeq(0x0000, 0x400, dest);
+	after = msec;
+	
+	uartWritePSTR("sram seq read speed: 1024 B / ");
+	uartWriteDec16(after - before);
+	uartWritePSTR(" ms\n");
+}
+
 static void testDRAM(void)
 {
 	const uint32_t chunkSize = 0x8000;
-	const uint16_t delayTime = 1000;
+	const uint16_t delayTime = 100;
 	
 	uint16_t errors = 0;
 	uint32_t min = 0xffffff, max = 0x000000;
 	
-	uartWritePSTR("dram test: 32KiB chunks, incr addr, alt aa/55, 1s wait, "
-		"10us spacing...\n");
+	/*for (uint32_t addr = 0x000000; addr < DRAM_SIZE; addr += 0x10)
+	{
+		uartWritePSTR("\n0x");
+		uartWriteHex24(addr, false);
+		uartWriteChr(':');
+		
+		for (uint8_t off = 0; off < 0x10; ++off)
+		{
+			uartWriteChr(' ');
+			uartWriteHex8(dramRead(addr + off), false);
+		}
+	}*/
+	
+	/*for (uint32_t addr = 0x03f000; addr < 0x03f800; addr += 0x100)
+	{
+		uint8_t val = 0;
+		
+		uartWritePSTR("\n0x");
+		uartWriteHex24(addr, false);
+		
+		do
+		{
+			dramWrite(addr, val);
+			uint8_t byte = dramRead(addr);
+			
+			if (byte == val)
+			{
+				uartWriteChr(' ');
+				uartWriteHex8(val, false);
+			}
+		}
+		while (--val != 0);
+	}*/
+	
+	uartWritePSTR("dram test: 32KiB chunks, incr addr, pat 55/aa, 100ms wait, "
+		"1us spacing...\n");
 	for (uint32_t chunk = 0x000000; chunk < 0x1000000; chunk += chunkSize)
 	{
-		uint16_t chunkErrors = 0;
+		uint16_t chunkErrors;
+		
+	redo:
+		chunkErrors = 0;
 		
 		uartWritePSTR("chunk 0x");
 		uartWriteHex24(chunk, false);
@@ -64,10 +125,10 @@ static void testDRAM(void)
 		{
 			uint32_t addr = chunk + relAddr;
 			
-			dramWrite(addr, 0xaa);
-			_delay_us(10);
-			dramWrite(addr + 1, 0x55);
-			_delay_us(10);
+			dramWrite(addr, 0x55);
+			_delay_us(1);
+			dramWrite(addr + 1, 0xaa);
+			_delay_us(1);
 		}
 		
 		uartWritePSTR(" waiting");
@@ -78,26 +139,43 @@ static void testDRAM(void)
 		for (uint32_t relAddr = 0x000000; relAddr < chunkSize; relAddr += 2)
 		{
 			uint32_t addr = chunk + relAddr;
+			uint8_t byte;
 			
-			if (dramRead(addr) != 0xaa)
+			if ((byte = dramRead(addr)) != 0x55)
 			{
 				if (addr < min)
 					min = addr;
 				if (addr > max)
 					max = addr;
 				++chunkErrors;
+				
+				/*uartWritePSTR("\nerror @ 0x");
+				uartWriteHex24(addr, false);
+				uartWritePSTR(": exp 0x");
+				uartWriteHex8(0x55, true);
+				uartWritePSTR(" act 0x");
+				uartWriteHex8(byte, true);
+				uartWriteChr(' ');*/
 			}
-			_delay_us(10);
+			_delay_us(1);
 			
-			if (dramRead(addr + 1) != 0x55)
+			if ((byte = dramRead(addr + 1)) != 0xaa)
 			{
 				if (addr < min)
 					min = addr;
 				if (addr > max)
 					max = addr;
 				++chunkErrors;
+				
+				/*uartWritePSTR("\nerror @ 0x");
+				uartWriteHex24(addr + 1, false);
+				uartWritePSTR(": exp 0x");
+				uartWriteHex8(0xaa, true);
+				uartWritePSTR(" act 0x");
+				uartWriteHex8(byte, true);
+				uartWriteChr(' ');*/
 			}
-			_delay_us(10);
+			_delay_us(1);
 		}
 		
 		errors += chunkErrors;
@@ -105,28 +183,31 @@ static void testDRAM(void)
 		uartWritePSTR(" done (");
 		uartWriteDec16(chunkErrors);
 		uartWritePSTR(" errors).\n");
+		
+		if (chunkErrors > 0)
+			goto redo;
 	}
 	
-	#if 0
+#if 0
 	for (uint32_t i = 0xffffff; (int32_t)i >= 0; --i)
-		dramWrite(i, i);
+		dramWrite(i, 0xff);
 	_delay_ms(1000);
 	for (uint32_t i = 0xffffff; (int32_t)i >= 0; --i)
 	{
 		uint8_t byte = dramRead(i);
 		
-		if (byte != (i & 0xff))
+		if (byte != 0xff)
 		{
-			/*uartWritePSTR("failed @ 0x");
+			uartWritePSTR("failed @ 0x");
 			uartWriteHex24(i, false);
 			uartWritePSTR(" (expected: 0x");
-			uartWriteHex8(i & 0xff, false);
+			uartWriteHex8(0xff, false);
 			uartWritePSTR(" actual: 0x");
 			uartWriteHex8(byte, false);
-			uartWritePSTR(")\n");*/
+			uartWritePSTR(")\n");
 			
-			uartWriteHex24(i, false);
-			uartWriteChr('\n');
+			/*uartWriteHex24(i, false);
+			uartWriteChr('\n');*/
 			
 			if (i < min)
 				min = i;
@@ -136,7 +217,6 @@ static void testDRAM(void)
 			++errors;
 		}
 	}
-	#endif
 	
 	uartWriteDec16(errors);
 	uartWritePSTR(" total errors (min: 0x");
@@ -144,6 +224,7 @@ static void testDRAM(void)
 	uartWritePSTR(", max: 0x");
 	uartWriteHex24(max, false);
 	uartWritePSTR(")\n");
+#endif
 }
 
 static void benchmarkDRAM(void)
@@ -156,10 +237,8 @@ static void benchmarkDRAM(void)
 	after = msec;
 	
 	uartWritePSTR("dram read speed: 64 KiB / ");
-	uartWriteDec16((after - before) / 1000);
-	uartWriteChr('.');
-	uartWriteDec16((after - before) % 1000);
-	uartWritePSTR(" s\n");
+	uartWriteDec16(after - before);
+	uartWritePSTR(" ms\n");
 	
 	before = msec;
 	for (uint32_t i = 0x000000; i < 0x00ffff; ++i)
@@ -167,10 +246,8 @@ static void benchmarkDRAM(void)
 	after = msec;
 	
 	uartWritePSTR("dram write speed: 64 KiB / ");
-	uartWriteDec16((after - before) / 1000);
-	uartWriteChr('.');
-	uartWriteDec16((after - before) % 1000);
-	uartWritePSTR(" s\n");
+	uartWriteDec16(after - before);
+	uartWritePSTR(" ms\n");
 }
 
 static void testRefresh(void)
@@ -191,10 +268,15 @@ void testAll(void)
 {
 	uartWritePSTR("-------- Unit Tests --------\n");
 	
+	/* write to ram */
+	for (uint32_t addr = 0x000000; addr < 0x000100; ++addr)
+		dramWrite(addr, 0xaa);
+	
 	testSRAM();
-	//benchmarkDRAM();
-	//testRefresh();
-	//testDRAM();
+	benchmarkSRAM();
+	benchmarkDRAM();
+	testRefresh();
+	testDRAM();
 	
 	/*writeReg(0b000, 0, 0x12345678, SIZE_LONG);
 	writeReg(0b000, 1, 0x1234, SIZE_WORD);
