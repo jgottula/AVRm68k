a886ddf867cd43044ffe0265df9d293f06a5d5db
Justin Gottula <justin@jgottula.com>
2012-07-10 18:16:26 -0700

    Programs: updated to use bsr, bra, and rts

--- programs/test1.s
+++ programs/test1.s
@@ -3,27 +3,29 @@
 	.equ EMU_DUMPREG, 0xa001
 	.equ EMU_DUMPMEM, 0xa002
 	
+	/* init stack ptr */
 	lea (0x40),%sp
-	pea 0xdeadbeef
 	
 	moveq #-0x40,%d0
 	movea.l (0x3c),%a6
 	
 	moveq #-0x01,%d0
 	move.w %d0,%ccr
-	move.w %sr,%d1
+	move.w %sr,%d1   
+	move.w %ccr,%d1
 	
 	movea.l (0x00000000),%a5
-	move.l (0x00000000),%a5
+	move.l (0x00000000),%a6
 	
-	move.l #0x05050505,%d2
-	move.b #0x66,%d2
-	move.l %d2,%a2
+	move.l #0x05050505,%d3
+	move.b #0x66,%d4
+	move.l %d2,%a5
 	
-	move.l #0xffffffff,%d0
-	not.l %d0
+	move.l #0xaaaaaaaa,%d6
+	not.l %d6
 	
-	jsr subroutine
+	bsr subroutine
+	jmp done
 	
 subroutine:
 	nop
@@ -31,6 +33,7 @@ subroutine:
 	nop
 	rts
 	
+done:
 	.word EMU_DUMPREG
 	.word EMU_DUMPMEM
 		.long 0x00000000
