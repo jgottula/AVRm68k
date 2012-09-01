c7d8648d4757c9a41180c89e826967e0f285c091
Justin Gottula <justin@jgottula.com>
2012-08-31 22:25:01 -0700

    Bitwise: added a #warning to getStatusReg because it needs to die

--- bitwise.h
+++ bitwise.h
@@ -14,6 +14,15 @@ uint32_t signExtend16to32(uint16_t word);
 
 inline uint8_t getStatusReg(void)
 {
+#warning "getStatusReg is horrible and needs to die"
+	
+	/* this is probably the worst possible way to get condition flags after a
+	 * math operation: it totally depends on the last instruction before this
+	 * function being the math instruction itself (which could depend on
+	 * optimization level), and 16-/32-bit operations frequently involve
+	 * multiple instructions, so it's questionable whether the last instruction
+	 * will have the proper flags in these cases */
+	
 	/* ensure proper ordering of instructions with a memory barrier */
 	asm volatile("" ::: "memory");
 	
