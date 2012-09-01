ce584c2af6f8f12be314aff86bfe317d9faeb313
Justin Gottula <justin@jgottula.com>
2012-09-01 00:02:51 -0700

    Bitwise: successfully jettisoned getStatusReg

--- bitwise.h
+++ bitwise.h
@@ -45,21 +45,4 @@ inline uint32_t shiftRightArithLong(uint32_t *operand, uint8_t shifts)
 	return (*operand = shifted);
 }
 
-inline uint8_t getStatusReg(void)
-{
-#warning "getStatusReg is horrible and needs to die"
-	
-	/* this is probably the worst possible way to get condition flags after a
-	 * math operation: it totally depends on the last instruction before this
-	 * function being the math instruction itself (which could depend on
-	 * optimization level), and 16-/32-bit operations frequently involve
-	 * multiple instructions, so it's questionable whether the last instruction
-	 * will have the proper flags in these cases */
-	
-	/* ensure proper ordering of instructions with a memory barrier */
-	asm volatile("" ::: "memory");
-	
-	return SREG;
-}
-
 #endif
