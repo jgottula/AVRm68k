12ea7c5b6ee9194f70b94362253d8ddc4a65a1f7
Justin Gottula <justin@jgottula.com>
2012-09-01 13:19:17 -0700

    Bitwise: replaced inline shift functions with new assembly routine

--- bitwise.h
+++ bitwise.h
@@ -12,37 +12,7 @@ uint16_t signExtend8to16(uint8_t byte);
 uint32_t signExtend8to32(uint8_t byte);
 uint32_t signExtend16to32(uint16_t word);
 
-/* hack: the C spec says that whether right shifts on signed integers are
- * arithmetic or logical shifts is implementation-defined; we rely on the former
- * behavior for these functions, so we check for it
- * 
- * potential alternate approaches include writing the routine in assembly, or
- * doing it horrendously slowly in software */
-_Static_assert(((int8_t)0x80 >> 1) == (int8_t)0xc0,
-	"compiler discards MSB on signed right shifts!");
-
-inline uint8_t shiftRightArithByte(uint8_t *operand, uint8_t shifts)
-{
-	int8_t signedOp = (int8_t)(*operand);
-	uint8_t shifted = (uint8_t)(signedOp >> shifts);
-	
-	return (*operand = shifted);
-}
-
-inline uint16_t shiftRightArithWord(uint16_t *operand, uint8_t shifts)
-{
-	int16_t signedOp = (int16_t)(*operand);
-	uint16_t shifted = (uint16_t)(signedOp >> shifts);
-	
-	return (*operand = shifted);
-}
-
-inline uint32_t shiftRightArithLong(uint32_t *operand, uint8_t shifts)
-{
-	int32_t signedOp = (int32_t)(*operand);
-	uint32_t shifted = (uint32_t)(signedOp >> shifts);
-	
-	return (*operand = shifted);
-}
+/* assembly */
+uint32_t shiftRightArithLong(uint32_t operand, uint8_t shifts, uint8_t *flags);
 
 #endif
