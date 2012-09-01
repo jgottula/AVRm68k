635bfdd22fc1059392ad24f604550c49404a16c1
Justin Gottula <justin@jgottula.com>
2012-09-01 13:49:13 -0700

    Bitwise: improved right-shift, added left-shift assembly function

--- bitwise.s
+++ bitwise.s
@@ -7,31 +7,73 @@
  * - uint8_t   shifts  [r20]
  *             (must be >0)
  * - uint8_t  *flags   [r19:r18]
- *             (changed to zero upon carry, left as is otherwise)
+ *             (bit 0 is last bit shifted out)
  * returns:
  * - uint32_t result  [r25:r22]
  */
 shiftRightArithLong:
-	/* perform one right-shift: start with arith right shift so sign bits are
-	 * handled appropriately, then rotate all the remaining bits right, with the
-	 * last bit ending up in the carry flag */
+	clr r21
+	
+shiftRightArithLong_Loop:
+	/* perform one right-shift */
 	asr r25
 	ror r24
 	ror r23
 	ror r22
 	
 	dec r20
-	brne shiftRightArithLong
+	brne shiftRightArithLong_Loop
 	
-	/* only do the next part if the last shift pushed a one out to carry */
-	brcc shiftRightArithLong_Done
+	/* set flags bit 0 if last bit shifted was a one */
+	brcc shiftRightArithLong_NoCarry
+	sbr r21,0x01
 	
-	/* load the flags pointer into Z */
+shiftRightArithLong_NoCarry:
+	/* load the flags pointer into Z and store the flags value */
 	mov r31,r19
 	mov r30,r18
+	st Z,r21
 	
-	/* store zero in the byte pointed to by Z */
-	st Z,r1
+	ret
 	
-shiftRightArithLong_Done:
+	
+	.global shiftLeftArithLong
+/* parameters:
+ * - uint32_t  operand [r25:r22]
+ * - uint8_t   shifts  [r20]
+ *             (must be >0)
+ * - uint8_t  *flags   [r19:r18]
+ *             (bit 0 is last bit shifted out,
+ *              bit 1 indicates if msb changed at any time)
+ * returns:
+ * - uint32_t result  [r25:r22]
+ */
+shiftLeftArithLong:
+	clr r21
+	
+shiftLeftArithLong_Loop:
+	/* perform one left-shift */
+	lsl r22
+	rol r23
+	rol r24
+	rol r25
+	
+	/* set flags bit 1 if msb changed during this shift */
+	brvc shiftLeftArithLong_NoMSB
+	sbr r21,0x02
+	
+shiftLeftArithLong_NoMSB:
+	dec r20
+	brne shiftLeftArithLong_Loop
+	
+	/* set flags bit 0 if last bit shifted was a one */
+	brcc shiftLeftArithLong_NoCarry
+	sbr r21,0x01
+	
+shiftLeftArithLong_NoCarry:
+	/* load the flags pointer into Z and store the flags value */
+	mov r31,r19
+	mov r30,r18
+	st Z,r21
+	
 	ret
