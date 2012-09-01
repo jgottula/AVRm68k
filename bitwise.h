2a11ade06fe35fdfad7836f688ce68105009d651
Justin Gottula <justin@jgottula.com>
2012-08-31 22:59:49 -0700

    Bitwise: added byte and word variants for arithmetic right shift

--- bitwise.h
+++ bitwise.h
@@ -12,17 +12,27 @@ uint16_t signExtend8to16(uint8_t byte);
 uint32_t signExtend8to32(uint8_t byte);
 uint32_t signExtend16to32(uint16_t word);
 
+/* hack: the C spec says that whether right shifts on signed integers are
+ * arithmetic or logical shifts is implementation-defined; we rely on the former
+ * behavior for these functions, so we check for it
+ * 
+ * potential alternate approaches include writing the routine in assembly, or
+ * doing it horrendously slowly in software */
+_Static_assert(((int8_t)0x80 >> 1) == (int8_t)0xc0,
+	"compiler discards MSB on signed right shifts!");
+
+inline uint8_t shiftRightArithByte(uint8_t operand, uint8_t shifts)
+{
+	return (uint8_t)((int8_t)operand >> shifts);
+}
+
+inline uint16_t shiftRightArithWord(uint16_t operand, uint8_t shifts)
+{
+	return (uint16_t)((int16_t)operand >> shifts);
+}
+
 inline uint32_t shiftRightArithLong(uint32_t operand, uint8_t shifts)
 {
-	/* hack: the C spec says that the compiler gets to decide whether right
-	 * shifts on signed integers are arithmetic or logical shifts; we rely on
-	 * the former behavior here, so we check for it
-	 * 
-	 * potential alternate approaches include writing the routine in assembly,
-	 * or doing it horrendously slowly in software */
-	_Static_assert(((int8_t)0x80 >> 1) == (int8_t)0xc0,
-		"compiler discards MSB on signed right shifts!");
-	
 	return (uint32_t)((int32_t)operand >> shifts);
 }
 
