5ecb45b0105fbc220df98bacec752c6a1654738b
Justin Gottula <justin@jgottula.com>
2012-09-02 01:04:00 -0700

    DMA: added interrupt code skeleton

---
+++ src/dma/intr.c
@@ -0,0 +1,9 @@
+#include "intr.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "io.h"
+
+void intrInit(void)
+{
+	
+}
---
+++ src/dma/intr.h
@@ -0,0 +1,9 @@
+#ifndef JGOTTULA_DMA_INTR_H
+#define JGOTTULA_DMA_INTR_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+void intrInit(void);
+
+#endif
