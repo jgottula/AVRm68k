f08117e90af87104085174772c97e72e72177b02
Justin Gottula <justin@jgottula.com>
2012-09-02 00:34:09 -0700

    IO: removed all IO definitions for DRAM (and RTC)

--- src/mcu/io.h
+++ src/mcu/io.h
@@ -9,40 +9,27 @@ typedef volatile uint8_t *reg_t;
 typedef volatile uint16_t *reg16_t;
 
 
-#define DDR_DATA  DDRA
-#define PORT_DATA PORTA
-#define PIN_DATA  PINA
+#if 0
+#define DDR_  DDRA
+#define PORT_ PORTA
+#define PIN_  PINA
 
 enum PortA {
-	DATA_0   = _BV(PA0),
-	DATA_1   = _BV(PA1),
-	DATA_2   = _BV(PA2),
-	DATA_3   = _BV(PA3),
-	DATA_4   = _BV(PA4),
-	DATA_5   = _BV(PA5),
-	DATA_6   = _BV(PA6),
-	DATA_7   = _BV(PA7),
-	DATA_ALL = DATA_0 | DATA_1 | DATA_2 | DATA_3 |
-	           DATA_4 | DATA_5 | DATA_6 | DATA_7
+	/* PA0-PA7 free */
 };
+#endif
 
-
-#define DDR_DRAM  DDRB
-#define PORT_DRAM PORTB
-#define PIN_DRAM  PINB
 
 #define DDR_SPI   DDRB
 #define PORT_SPI  PORTB
 #define PIN_SPI   PINB
 
 enum PortB {
-	DRAM_CAS    = _BV(PB0),
-	DRAM_WE     = _BV(PB1),
-	DRAM_RAS    = _BV(PB2),
-	DRAM_ALL    = DRAM_CAS | DRAM_WE | DRAM_RAS,
+	/* PB0 free */
 	
-	RTC_IO      = _BV(PB3),
-	RTC_ALL     = _BV(PB3),
+	/* PB1 reserved for CLKOUT */
+	
+	/* PB1-PB3 free */
 	
 	SPI_SS_NULL = _BV(PB4),
 	SPI_MOSI    = _BV(PB5),
@@ -52,22 +39,15 @@ enum PortB {
 };
 
 
-#define DDR_ADDRL  DDRC
-#define PORT_ADDRL PORTC
-#define PIN_ADDRL  PINC
+#if 0
+#define DDR_  DDRC
+#define PORT_ PORTC
+#define PIN_  PINC
 
 enum PortC {
-	ADDRL_0   = _BV(PC0),
-	ADDRL_1   = _BV(PC1),
-	ADDRL_2   = _BV(PC2),
-	ADDRL_3   = _BV(PC3),
-	ADDRL_4   = _BV(PC4),
-	ADDRL_5   = _BV(PC5),
-	ADDRL_6   = _BV(PC6),
-	ADDRL_7   = _BV(PC7),
-	ADDRL_ALL = ADDRL_0 | ADDRL_1 | ADDRL_2 | ADDRL_3 |
-	            ADDRL_4 | ADDRL_5 | ADDRL_6 | ADDRL_7
+	/* PC0-PC7 free */
 };
+#endif
 
 
 #define DDR_UART  DDRD
@@ -83,7 +63,7 @@ enum PortD {
 	UART_TX    = _BV(PD1),
 	UART_ALL   = UART_RX | UART_TX,
 	
-	/* PD2 and PD3 are open */
+	/* PD2-PD3 free */
 	
 	SREG_CLEAR = _BV(PD4),
 	SREG_DATA  = _BV(PD5),
