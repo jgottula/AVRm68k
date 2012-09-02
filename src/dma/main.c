e33e780d8bc1bc9191991957a8e39edcd26c1406
Justin Gottula <justin@jgottula.com>
2012-09-02 01:05:08 -0700

    DMA: added references to new code

--- src/dma/main.c
+++ src/dma/main.c
@@ -1,8 +1,13 @@
 #include <stdint.h>
 #include <stdnoreturn.h>
+#include "debug.h"
+#include "dram.h"
+#include "intr.h"
 #include "io.h"
 
 noreturn void main(void)
 {
+	intrInit();
+	
 	for ( ;; );
 }
