3b4f12ce79533314f3a443a59f1d3fe5d7a28aba
Justin Gottula <justin@jgottula.com>
2012-09-07 19:31:17 -0700

    Scripts: added file header

--- scripts/avrterminal.sh
+++ scripts/avrterminal.sh
@@ -1,5 +1,10 @@
-#!/bin/bash
+# AVRm68k
+# (c) 2012 Justin Gottula
+# The source code of this project is distributed under the terms of the
+# simplified BSD license. See the LICENSE file for details.
+
+# avrdude terminal script
 
 sudo rmmod -f ftdi_sio
-sudo avrdude -p m1284p -c usbasp -B 460800 -t
+sudo avrdude -p m1284p -c usbasp -B 230400 -t
 sudo modprobe ftdi_sio
--- scripts/screen.sh
+++ scripts/screen.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
+# AVRm68k
+# (c) 2012 Justin Gottula
+# The source code of this project is distributed under the terms of the
+# simplified BSD license. See the LICENSE file for details.
 
-sudo screen /dev/ftdifriend 1250000
+# screen terminal script
+
+sudo screen /dev/ftdifriend 230400
