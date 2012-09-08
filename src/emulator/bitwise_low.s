fdf4966064ff0003277869edb37d39dc35e542be
Justin Gottula <justin@jgottula.com>
2012-09-07 22:47:59 -0700

    Bitwise: added header to assembly source file

--- src/emulator/bitwise_low.s
+++ src/emulator/bitwise_low.s
@@ -1,3 +1,8 @@
+/* AVRm68k [emulator subproject]
+ * (c) 2012 Justin Gottula
+ * The source code of this project is distributed under the terms of the
+ * simplified BSD license. See the LICENSE file for details. */
+
 	.section .text
 	
 	
