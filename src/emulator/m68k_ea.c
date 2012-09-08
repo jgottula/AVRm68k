8d56110a375ca81e7b9f9c5d1d8032e31210135f
Justin Gottula <justin@jgottula.com>
2012-09-07 23:21:05 -0700

    Header: fixed bad spacing

--- src/emulator/m68k_ea.c
+++ src/emulator/m68k_ea.c
@@ -2,8 +2,8 @@
  * (c) 2012 Justin Gottula
  * The source code of this project is distributed under the terms of the
  * simplified BSD license. See the LICENSE file for details. */
- 
- #include "all.h"
+
+#include "all.h"
 
 static bool extWordType(const uint8_t *ptr)
 {
