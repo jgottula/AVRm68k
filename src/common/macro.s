84ce9b350fcbb7cdea1a62a392d4a0d42b145f3d
Justin Gottula <justin@jgottula.com>
2012-09-09 02:01:26 -0700

    Macros: added macros for pushing/popping the call-clobbered registers

--- src/common/macro.s
+++ src/common/macro.s
@@ -6,6 +6,8 @@
 	.altmacro
 	
 	
+	/* push the registers numbered from low to high, inclusive, onto the stack
+	 * in ascending order */
 	.macro savereg low high
 	
 	push r\low
@@ -17,6 +19,8 @@
 	.endm
 	
 	
+	/* pop the registers numbered from high to low, inclusive, from the stack in
+	 * descending order */
 	.macro restreg high low
 	
 	pop r\high
@@ -24,5 +28,23 @@
 	.if (\high - \low) != 0
 		restreg %(\high - 1),\low
 	.endif
+	
+	.endm
+	
+	
+	/* save all call-clobbered registers (only useful for ISRs) */
+	.macro saveclobber
+	
+	savereg 18,27
+	savereg 30,31
+	
+	.endm
+	
+	
+	/* restore all call-clobbered registers (only useful for ISRs) */
+	.macro restclobber
+	
+	restreg 31,30
+	restreg 27,18
 	
 	.endm
